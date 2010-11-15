1f857a178cb76a8cc6a1d13e571a7cf01899b55a
theoalves <theoalves@8b08d114-957c-4e89-a9e4-e2b9325cd9c1>
2010-11-15 19:50:30 +0000

    Iniciando a implementação do algoritmo mostrado no artigo bem como um TMesh simples baseado em coleção de Knot

---
+++ trunk/adapTSpline/src/br/edu/ufcg/dsc/msn/core/Knot.java
@@ -0,0 +1,20 @@
+package br.edu.ufcg.dsc.msn.core;
+
+public class Knot {
+	
+	private double s, t;
+	
+	public Knot(double s, double t) {
+		this.s = s;
+		this.t = t;
+	}
+	
+	public double getS() {
+		return s;
+	}
+
+	public double getT() {
+		return t;
+	}
+
+}
