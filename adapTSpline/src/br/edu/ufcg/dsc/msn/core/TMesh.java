06cb8fa1f9751ce51dc4029b0bd3ff86d6a45699
theoalves <theoalves@8b08d114-957c-4e89-a9e4-e2b9325cd9c1>
2010-11-15 19:50:30 +0000

    Iniciando a implementação do algoritmo mostrado no artigo bem como um TMesh simples baseado em coleção de Knot

--- adapTSpline/src/br/edu/ufcg/dsc/msn/core/TMesh.java
+++ adapTSpline/src/br/edu/ufcg/dsc/msn/core/TMesh.java
@@ -1,5 +1,49 @@
 package br.edu.ufcg.dsc.msn.core;
 
-public interface TMesh extends BiDimensionalFunction {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class TMesh {
+	
+	private List<Knot> knotsInS;
+	
+	public TMesh() {
+		this.knotsInS = new ArrayList<Knot>();
+	}
+	
+	public void add(Knot knot) {
+		Iterator<Knot> i = knotsInS.iterator();
+		int pos = 0;
+		while(i.hasNext()) {
+			if(i.next().getS() < knot.getS())
+				pos++;
+			else break;
+		}
+		this.knotsInS.add(pos, knot);
+	}
+
+	public double z(double x, double y) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	public List<Knot> boundingBox(Knot knot) {
+		List<Knot> square = new ArrayList<Knot>();
+		if(!this.knotsInS.isEmpty()) {
+			Knot p1, p2, p3, p4;
+			Iterator<Knot> i = this.knotsInS.iterator();
+			p1 = p2 = p3 = p4 = i.next();
+			while(i.hasNext()) {
+				
+			}
+			square.add(p1);
+			square.add(p2);
+			square.add(p3);
+			square.add(p4);
+		}
+		return square;
+	}
+
 
 }
--- adapTSpline/src/br/edu/ufcg/dsc/msn/core/ZMap2TSplines.java
+++ adapTSpline/src/br/edu/ufcg/dsc/msn/core/ZMap2TSplines.java
@@ -1,8 +1,77 @@
 package br.edu.ufcg.dsc.msn.core;
 
-public interface ZMap2TSplines {
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class ZMap2TSplines {
 	
-	//Cria o TMesh segundo o algoritmo
-	public TMesh create(ZMap map);
+	private double tolerance, omega, lastMax;
+	public static final double NU = 0.2;
+	public static final double MI = 0.9;
+	
+	public ZMap2TSplines(double tolerance, double omega) {
+		this.tolerance = tolerance;
+		this.omega = omega;
+	}
+	
+	public TMesh create(ZMap map) {
+		TMesh mesh = new TMesh();
+		this.initialize(mesh, map);
+		this.optimization(mesh, map);
+		while(!this.check(mesh, map).isEmpty()) {
+			this.refine(mesh, map);
+			this.optimization(mesh, map);
+		}
+		return mesh;
+	}
+	
+	private void refine(TMesh tMesh, ZMap zMap) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private Collection<Knot> check(TMesh tMesh, ZMap zMap) {
+		Collection<Knot> errors = new ArrayList<Knot>();
+		double currentMax = 0;
+		for(int i = 0; i < zMap.width(); i++) {
+			for(int j = 0; j < zMap.height(); j++) {
+				double distance = Math.abs(tMesh.z((double)i, (double)j) - zMap.z(i, j));
+				if( distance > this.tolerance) {
+					errors.add(new Knot(i, j));
+				}
+				if (distance > currentMax) {
+					currentMax = distance;
+				}
+			}
+			if (lastMax - currentMax > MI) {
+				double tmp = this.omega - NU;
+				this.omega = tmp > 0 ? tmp : 0;
+			}
+		}
+		return errors;
+	}
+	
+	private void optimization(TMesh tMesh, ZMap zmap){
+		
+	}
+	
+	private void initialize(TMesh tMesh, ZMap zMap) {
+		tMesh.add(new Knot(0, 0));
+		tMesh.add(new Knot(0, 1));
+		tMesh.add(new Knot(0, zMap.height()));
+		tMesh.add(new Knot(0, zMap.height() + 1));
+		tMesh.add(new Knot(1, 0));
+		tMesh.add(new Knot(1, 1));
+		tMesh.add(new Knot(1, zMap.height()));
+		tMesh.add(new Knot(1, zMap.height() + 1));
+		tMesh.add(new Knot(zMap.width(), 0));
+		tMesh.add(new Knot(zMap.width(), 1));
+		tMesh.add(new Knot(zMap.width(), zMap.height()));
+		tMesh.add(new Knot(zMap.width(), zMap.height() + 1));
+		tMesh.add(new Knot(zMap.width() + 1, 0));
+		tMesh.add(new Knot(zMap.width() + 1, 1));
+		tMesh.add(new Knot(zMap.width() + 1, zMap.height()));
+		tMesh.add(new Knot(zMap.width() + 1, zMap.height() + 1));
+	}
 
 }
